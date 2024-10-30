403f81d5a67616291a96f017051b587df586527a
claudiopatti <claudiopatti1997@gmail.com>
2024-10-30 17:41:41 +0100

    aggiunto update e delete per la foreign di type in project

--- database/migrations/2024_10_29_162848_add_type_id_to_projects_table.php
+++ database/migrations/2024_10_29_162848_add_type_id_to_projects_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
 
             $table->foreign('type_id')
                     ->references('id')
-                    ->on('types');
+                    ->on('types')
+                    ->onUpdate('cascade')
+                    ->onDelete('set null');
         });
     }
 
